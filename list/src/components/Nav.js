f62b677cb33e8686df5b97ccde8aff0d52ad9e82
sohaib-albyari <salbyari3@gmail.com>
2024-01-17 09:23:25 +0300

    fixed the user name

--- list/src/components/Nav.js
+++ list/src/components/Nav.js
@@ -6,19 +6,21 @@ function Nav(props) {
   return (
     <>
       <nav
-        style={{ display: props.name === undefined ? "none" : "block" }}
+        style={{ display: props.name === undefined ? "none" : "flex" }}
         className="navbar"
         id="h"
       >
-        <div className="container">
+        <div className="container-fluid">
           <div className="row">
             <div className="col-2">
               <Link to={"/task"} className="navbar-brand">
                 <img className="float-start w-50" src={logo} alt="0" />
               </Link>
             </div>
-            <div className="col-6">
-              <h1 className="float-end">Welcome {props.name}</h1>
+            <div className="welcomeMsg col-10">
+              <h1 className="float-end text-white-50 text-center m-auto">
+                Welcome {props.name}
+              </h1>
             </div>
           </div>
         </div>
